ea2d1172a267f873c3a2c601fe3c05cce4b2b9ff
xQiwix <159818062+xQiwix@users.noreply.github.com>
2024-03-24 21:35:18 +0300

    Update alg1.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,4 +3,30 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
+  int left = 0;
+    int right = size - 1;
+    int cnt = 0;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value) {
+            cnt++;
+            int i = mid - 1;
+            while (i >= 0 && arr[i] == value) {
+                cnt++;
+                i--;
+            }
+            int j = mid + 1;
+            while (j < size && arr[j] == value) {
+                cnt++;
+                j++;
+            }
+            return count;
+        } else if (arr[mid] < value) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+  return 0;
 }
